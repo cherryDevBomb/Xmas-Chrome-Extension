b81a79b56a7152025a69f163c12b15c486f497a9
cherryDevBomb <turcan.olguta@gmail.com>
2020-07-16 00:09:11 +0300

    clean up src folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  /!*<p>*!/*/}
-      {/*  /!*  Edit <code>src/App.js</code> and save to reload.*!/*/}
-      {/*  /!*</p>*!/*/}
-      {/*</header>*/}
-    </div>
+    <div className="App" />
   );
 }
 
